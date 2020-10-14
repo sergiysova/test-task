6bcf3fd6f5a55b3a70abd4b80ba1e0e79329d2fa
= <serj.sova@gmail.com>
2020-10-14 17:50:50 +0300

    builded production

---
+++ build/precache-manifest.303ee159e967f2b3fbc94cffa971d423.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "842841f1b4bb105608ba9dcd5ace0a96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a49309eb5527f998626",
+    "url": "/static/css/main.b9582262.chunk.css"
+  },
+  {
+    "revision": "20395522d7b3c2118d49",
+    "url": "/static/js/2.8fc12648.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.8fc12648.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a49309eb5527f998626",
+    "url": "/static/js/main.604847a9.chunk.js"
+  },
+  {
+    "revision": "3c723e48decc6f6f72c1",
+    "url": "/static/js/runtime-main.30aace72.js"
+  },
+  {
+    "revision": "f5443d74025f2a6069e0ca0142c08a05",
+    "url": "/static/media/banner-photo.f5443d74.jpg"
+  },
+  {
+    "revision": "146e29265bcc680129e263a9478dca00",
+    "url": "/static/media/logo.146e2926.svg"
+  },
+  {
+    "revision": "3cdf78971c869b16b95152be7b6922e5",
+    "url": "/static/media/man-laptop-v1.3cdf7897.svg"
+  },
+  {
+    "revision": "6f6bef288009ad20ca333f0cd61608c6",
+    "url": "/static/media/menu icon.6f6bef28.svg"
+  }
+]);
